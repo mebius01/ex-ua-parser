a6cc486144d2d4263c1cad19c99088884e2cbdc8
mebius <mebius@hiruba.(none)>
2013-10-06 19:31:58 +0300

    add ex.py

---
+++ ex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#A="[a-z:/[a-z./[0-9/[abcdef0-9]+.flv"
+#B=re.compile(A)
+#C=re.findall(
+import urllib, urllib2, re, sys, os
+html = 'http://www.ex.ua/21522453?r=23786'
+Url_html=urllib.urlopen(html)
+Read_html=Url_html.read()
+List_re=re.findall("[http://www.ex.ua/show/0-9abcdef0-9]+.flv", Read_html)
+
+for i in List_re:
+	os.system('wget %s' %i)
