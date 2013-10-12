1d11b8de188179fb98a449fa7e55a4db6f79e754
mebius <mebius@hiruba.(none)>
2013-10-12 14:43:41 +0300

    ..

--- ex.py
+++ ex.py
@@ -5,15 +5,15 @@
 #B=re.compile(A)
 #C=re.findall(
 import urllib, urllib2, re, sys, os
-html = 'http://www.ex.ua/21522453?r=23786'
+#~ 
+html = 'http://www.ex.ua/73222171?r=23786,23775'; html=str(html)
+#~ tit_le= raw_input('name title: '); tit_le=str(tit_le)
+#~ 
 Url_html=urllib.urlopen(html)
 Read_html=Url_html.read()
-List_re=re.findall("[http://www.ex.ua/show/0-9abcdef0-9]+.flv", Read_html)
-
-for i in List_re:
-	#os.system('wget %s' %i)
-	print i
-List_reII=re.findall("[\[A-D\] | 0-9].+mp4", Read_html)
-
-for ii in List_reII:
-	print ii
+List_re=re.findall("http://www.ex.ua/show/0-9abcdef0-9+.flv", Read_html)
+#~ 
+#~ for i in List_re:
+	#~ i=str(i)
+	#~ print i
+print List_re
