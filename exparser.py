24164b4ba181b43e60f76679f5575819b9c525d2
mebius <mebius@hiruba.(none)>
2013-10-17 23:12:26 +0300

    rm def PopListTitle

--- exparser.py
+++ exparser.py
@@ -9,6 +9,7 @@ Read_html=Url_html.read()
 List_re=re.findall("[http://www.ex.ua/show/0-9abcdef0-9]+.flv", Read_html)
 List_re_dir=re.findall("[0-9a-z]+.flv", Read_html) 
 Title_re=re.search('(?<=<title>).*?(?=\/)', Read_html).group()
+image=re.search('(?<="cover": ").*(?=\/)', Read_html).group()
 
 os.mkdir(Title_re)
 os.chdir(Title_re)
@@ -18,15 +19,15 @@ def ForTitle():
 	ran=range(len(List_re)+1); ran.pop(0)
 	for i in ran:
 		i=str(i)
-		list_title.append(i+'-'+Title_re)
+		list_title.append(i+'-'+Title_re+'.flv')
 	return list_title
 
-def PopListTitle(lis):
-	if len(lis) >=0:
-		d=lis.pop(0)
-		print d
-	elif len(lis) ==0:
-		print 'list пуст!!!!!!'
+#~ def PopListTitle(lis):
+	#~ if len(lis) >=0:
+		#~ d=lis.pop(0)
+		#~ print d
+	#~ elif len(lis) ==0:
+		#~ print 'list пуст!!!!!!'
 
 ForTitle()
 
@@ -44,4 +45,4 @@ while int != len(List_re):
 	FunWget(List_re)
 	RenaMe(List_re_dir, list_title)
 	int+1
-
+os.system('wget %s' %image)
